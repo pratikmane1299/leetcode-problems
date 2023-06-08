cafffdb136006b4694feded456be9609653b090a
Pratik Mane <pratik.mane1299@gmail.com>
2023-06-08 23:33:36 +0530

    june 8 problems done

---
+++ 1351-count-negatives-in-sorted-matrix.js
@@ -0,0 +1,26 @@
+function countNegatives(grid) {
+  let count = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] < 0) count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(
+  countNegatives([
+    [4, 3, 2, -1],
+    [3, 2, 1, -1],
+    [1, 1, -1, -2],
+    [-1, -1, -2, -3],
+  ])
+);
+console.log(
+  countNegatives([
+    [3, 2],
+    [1, 0],
+  ])
+);
